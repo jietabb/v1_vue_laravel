bdaef655d5e2016b317c443feb724f9e8383a94c
jietabb <wanghuajie@x-service.co.jp>
2022-10-05 00:48:14 +0900

    call create api work

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 // Route::get('/tasks', 'TaskController@index');
 Route::get('/tasks', [TaskController::class, 'index']);
-Route::post('/tasks/{task}', [TaskController::class, 'store']);
+Route::post('/tasks', [TaskController::class, 'store']);
 Route::get('/tasks/{task}', [TaskController::class, 'show']);
 Route::put('/tasks/{task}', [TaskController::class, 'update']);
 Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
