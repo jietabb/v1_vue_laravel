ac24ab1eaf559e692ef84474d60cb5e5d236e9c4
jietabb <wanghuajie@x-service.co.jp>
2022-10-05 00:20:29 +0900

    add api for single task

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 // Route::get('/tasks', 'TaskController@index');
 Route::get('/tasks', [TaskController::class, 'index']);
+Route::get('/tasks/{task}', [TaskController::class, 'show']);
